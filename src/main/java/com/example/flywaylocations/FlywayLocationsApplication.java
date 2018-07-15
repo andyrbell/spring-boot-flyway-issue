1124c5c8fe44bca44d1c5cb46b5394375cb553d6
Andy Bell <andyrbell@gmail.com>
2018-07-15 23:47:09 +0100

    initial

---
+++ src/main/java/com/example/flywaylocations/FlywayLocationsApplication.java
@@ -0,0 +1,12 @@
+package com.example.flywaylocations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayLocationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayLocationsApplication.class, args);
+	}
+}
